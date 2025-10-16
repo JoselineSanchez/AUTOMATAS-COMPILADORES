b3d3c58d94e8d318b4fbccc2a20e7bde7703d084
JoselineSanchez <lo489516@uaeh.edu.mx>
2025-10-16 10:19:37 -0600

    Add files via upload

---
+++ practico/prueba.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Este es un comentario
+    int x = 10;
+    float y = 3.5;
+    if (x > 0 && y != 0) {
+        x = x + 1;
+    }
+    return 0;
+}
