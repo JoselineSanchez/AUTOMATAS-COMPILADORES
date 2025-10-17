cc0253a4853b90925614afbce1a4562715b1a366
JoselineSanchez <lo489516@uaeh.edu.mx>
2025-10-17 10:41:34 -0600

    Add files via upload

---
+++ lexico/tokens.h
@@ -0,0 +1,14 @@
+
+	#define TOKEN_INICIO 1
+	#define TOKEN_FIN 2
+	#define TOKEN_VECTOR 3
+	#define TOKEN_ENTERO 4
+	#define TOKEN_LOGICO 5
+	#define TOKEN_JOSELINE 6
+
+	#define TAXONOMIA_BLOOM 7
+	#define ANALIZAR_BLOOM 8
+	#define ADJETIVO 9
+	#define CONJUNCION 10
+	#define PREPOSICION 11
+	#define PRONOMBRE 12
